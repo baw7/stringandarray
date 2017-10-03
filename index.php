954ceb2e3cfb1385a2ea0cd557dc5207d77409bc
baw7 <baw7@njit.edu>
2017-10-02 21:07:09 -0400

    test

--- index.php
+++ index.php
@@ -6,3 +6,9 @@ $a=array("a"=>"Red","b"=>"Yellow","c"=>"Blue");
 print_r(array_reverse($a));?>
 <hr>
 
+<h1>2. Uppercase</h1>
+<?php
+$color=array("Red"=>"1","Blue"=>"2","Yellow"=>"3");
+print_r(array_change_key_case($age,CASE_UPPER));?>
+<hr>
+
